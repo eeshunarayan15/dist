dc5f2ce1d8ff51ff2305d1c7c415d9f9a07622a2
eeshunarayan15 <106856120+eeshunarayan15@users.noreply.github.com>
2024-08-10 15:47:51 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const scroll = new LocomotiveScroll({
+        el: document.querySelector('#main'),
+        smooth: true,
+    });
+
+    console.log('Locomotive Scroll initialized:', scroll);
+});
