ac0b7d2076ff120d5743f4a7da834c6a34720bbd
eeshunarayan15 <106856120+eeshunarayan15@users.noreply.github.com>
2024-08-10 20:09:38 +0530

    updated html,css and javascript

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
 
 
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
-});
 
 
-document.addEventListener('DOMContentLoaded', () => {
-    const scroll = new LocomotiveScroll({
-        el: document.querySelector('#main'),
-        smooth: true,
-    });
 
-    console.log('Locomotive Scroll initialized:', scroll);
-});
+
+
+console.log("hello at first") 
+
+
+var abc=document.querySelector('#elm1')
+var image=(abc.getAttribute("data-image"))
+console.log(abc)
+
+
+var elmc=document.querySelector("#elm-container")
+var fixed=document.querySelector("#fixed-image")
+elmc.addEventListener("mouseenter",function(){
+fixed.style.display="block"
+console.log("mouse entered")
+})
+elmc.addEventListener("mouseleave",function(){
+    fixed.style.display="none"
+    console.log("mouse leave")
+})
+console.log("hello at last")
+// var elem1=document.querySelector("#elm1")
+// elem1.addEventListener("mouseenter",function(){
+// var image=elem1.getAttribute("data-image");
+// console.log(image);
+// fixed.style.backgroundImage=`url(${image})`
+// })
+
+var elms=document.querySelectorAll(".elm")
+console.log(elms)
+elms.forEach(function(e){
+console.log(e)
+
+e.addEventListener("mouseenter",function(){
+    var image=e.getAttribute("data-image");
+    console.log(image);
+    fixed.style.backgroundImage=`url(${image})`
+})
+})
